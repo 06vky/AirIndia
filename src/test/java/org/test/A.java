abec33bd2203c6ba06abe239206ed070caad529f
Vignesh <vky3366@gmail.com>
2020-02-24 22:49:00 +0530

    Project ready

---
+++ src/test/java/org/test/A.java
@@ -0,0 +1,18 @@
+package org.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class A {
+	static WebDriver driver;
+	//@beforeclass
+	public static void launchBrowser() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\MYPC\\eclipse-workspace\\AirIndia\\driver\\chromedriver.exe");
+		driver=new ChromeDriver();
+	}
+	
+	
+	
+	
+
+}
